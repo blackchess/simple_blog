2984e68521f9b760c4d83183460f14767ee7c7d3
blackchess <2831486098@qq.com>
2023-05-28 11:33:51 +0800

    初始化项目

---
+++ src/main/java/com/liaoxin/SimpleBlogApplication.java
@@ -0,0 +1,13 @@
+package com.liaoxin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liaoxin/SimpleBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liaoxin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
